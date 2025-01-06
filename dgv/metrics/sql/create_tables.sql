datagouv/datagouvfr_data_pipelines

feat(metrics): add dataservices metrics
And refactor of the code to make it slightly more modular and easier to
navigate.
And add a split between languages and API segments.

BREAKING CHANGE:
Migration of a few tables to add new columns.

dgv/metrics/sql/create_tables.sql
@@ -9,7 +9,15 @@ CREATE TABLE IF NOT EXISTS metric.visits_datasets
     date_metric DATE,
     dataset_id CHARACTER VARYING,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
+
 );
 CREATE TABLE IF NOT EXISTS metric.visits_reuses
 (
@@ -17,14 +25,28 @@ CREATE TABLE IF NOT EXISTS metric.visits_reuses
     date_metric DATE,
     reuse_id CHARACTER VARYING,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS metric.visits_organizations
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS metric.visits_resources
 (
@@ -33,7 +55,30 @@ CREATE TABLE IF NOT EXISTS metric.visits_resources
     resource_id CHARACTER VARYING,
     dataset_id CHARACTER VARYING,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS metric.visits_dataservices
+(
+    __id SERIAL PRIMARY KEY,
+    date_metric DATE,
+    dataservice_id CHARACTER VARYING,
+    organization_id CHARACTER VARYING,
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
 );
 
 -- Matomo tables
@@ -60,12 +105,20 @@ CREATE TABLE IF NOT EXISTS metric.matomo_organizations
     organization_id CHARACTER VARYING,
     nb_outlink INTEGER
 );
+CREATE TABLE IF NOT EXISTS metric.matomo_dataservices
+(
+    __id SERIAL PRIMARY KEY,
+    date_metric DATE,
+    dataservice_id CHARACTER VARYING,
+    organization_id CHARACTER VARYING,
+    nb_outlink INTEGER
+);
 
 
 -- Aggregated metrics tables
 CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_datasets AS
     SELECT visits.__id as __id,
-            COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
+           COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
            COALESCE(visits.dataset_id, matomo.dataset_id) as dataset_id,
            COALESCE(visits.organization_id, matomo.organization_id) as organization_id,
            visits.nb_visit,
@@ -97,6 +150,19 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_reuses AS
        visits.date_metric = matomo.date_metric
 ;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_dataservices AS
+    SELECT visits.__id as __id,
+           COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
+           COALESCE(visits.dataservice_id, matomo.dataservice_id) as dataservice,
+           COALESCE(visits.organization_id, matomo.organization_id) as organization_id,
+           visits.nb_visit,
+           matomo.nb_outlink
+    FROM metric.visits_dataservices AS visits
+    FULL OUTER JOIN metric.matomo_dataservices AS matomo
+    ON visits.dataservice_id = matomo.dataservice_id AND
+       visits.date_metric = matomo.date_metric
+;
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_organizations AS
     SELECT visits.__id as __id,
            COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
@@ -122,6 +188,12 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_organizations AS
     ) reuses
     ON COALESCE(visits.organization_id, matomo.organization_id) = reuses.organization_id AND
        COALESCE(visits.date_metric, matomo.date_metric) = reuses.date_metric
+    LEFT OUTER JOIN (
+        SELECT organization_id, date_metric, sum(nb_visit) as nb_visit FROM metric.metrics_dataservices
+        GROUP BY organization_id, date_metric
+    ) dataservices
+    ON COALESCE(visits.organization_id, matomo.organization_id) = dataservices.organization_id AND
+       COALESCE(visits.date_metric, matomo.date_metric) = dataservices.date_metric
 ;
 
 -- Monthly aggregated metrics tables
@@ -169,6 +241,16 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.resources AS
     GROUP BY metric_month, resource_id, dataset_id
 ;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.dataservices AS
+    SELECT
+        MIN(__id) as __id,
+        dataservice_id,
+        to_char(date_trunc('month', date_metric) , 'YYYY-mm') AS metric_month,
+        sum(nb_visit) as monthly_visit
+    FROM metric.visits_dataservices
+    GROUP BY metric_month, dataservice_id
+;
+
 -- Global site table
 CREATE MATERIALIZED VIEW IF NOT EXISTS metric.site AS
     SELECT __id,
@@ -187,6 +269,11 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.site AS
         sum(monthly_visit) as monthly_visit
         FROM metric.reuses GROUP BY metric_month ) reuses
     ON datasets.metric_month = reuses.metric_month
+    FULL OUTER JOIN (
+        SELECT metric_month,
+        sum(monthly_visit) as monthly_visit
+        FROM metric.dataservices GROUP BY metric_month ) dataservices
+    ON datasets.metric_month = dataservices.metric_month
 ;
 
 -- Sum tables
@@ -233,6 +320,14 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.resources_total AS
     GROUP BY resource_id, dataset_id
 ;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.dataservices_total AS
+    SELECT
+        MIN(__id) AS __id,
+        dataservice_id,
+        SUM(nb_visit) AS visist_dataservice
+    FROM metric.visits_dataservices
+    GROUP BY dataservice_id
+;
 
 CREATE INDEX IF NOT EXISTS visits_datasets_dataset_id ON metric.visits_datasets USING btree (dataset_id);
 CREATE INDEX IF NOT EXISTS visits_datasets_date_metric ON metric.visits_datasets USING btree (date_metric);
@@ -248,3 +343,7 @@ CREATE INDEX IF NOT EXISTS visits_reuses_organization_id ON metric.visits_reuses
 CREATE INDEX IF NOT EXISTS visits_resources_resource_id ON metric.visits_resources USING btree (resource_id);
 CREATE INDEX IF NOT EXISTS visits_resources_date_metric ON metric.visits_resources USING btree (date_metric);
 CREATE INDEX IF NOT EXISTS visits_resources_dataset_id ON metric.visits_resources USING btree (dataset_id);
+
+CREATE INDEX IF NOT EXISTS visits_dataservices_dataservice_id ON metric.visits_dataservices USING btree (dataservice_id);
+CREATE INDEX IF NOT EXISTS visits_dataservices_organization_id ON metric.visits_dataservices USING btree (organization_id);
+CREATE INDEX IF NOT EXISTS visits_dataservices_date_metric ON metric.visits_dataservices USING btree (date_metric);

geoffreyaldebert/dag_datagouv_data_pipelines

dgv/metrics/sql/create_tables.sql
@@ -9,7 +9,15 @@ CREATE TABLE IF NOT EXISTS metric.visits_datasets
     date_metric DATE,
     dataset_id CHARACTER VARYING,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
+
 );
 CREATE TABLE IF NOT EXISTS metric.visits_reuses
 (
@@ -17,14 +25,28 @@ CREATE TABLE IF NOT EXISTS metric.visits_reuses
     date_metric DATE,
     reuse_id CHARACTER VARYING,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS metric.visits_organizations
 (
     __id SERIAL PRIMARY KEY,
     date_metric DATE,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS metric.visits_resources
 (
@@ -33,7 +55,30 @@ CREATE TABLE IF NOT EXISTS metric.visits_resources
     resource_id CHARACTER VARYING,
     dataset_id CHARACTER VARYING,
     organization_id CHARACTER VARYING,
-    nb_visit INTEGER
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS metric.visits_dataservices
+(
+    __id SERIAL PRIMARY KEY,
+    date_metric DATE,
+    dataservice_id CHARACTER VARYING,
+    organization_id CHARACTER VARYING,
+    nb_visit INTEGER DEFAULT 0,
+    nb_visit_apis INTEGER DEFAULT 0,
+    nb_visit_total INTEGER DEFAULT 0,
+    nb_visit_api1 INTEGER DEFAULT 0,
+    nb_visit_api2 INTEGER DEFAULT 0,
+    nb_visit_fr INTEGER DEFAULT 0,
+    nb_visit_en INTEGER DEFAULT 0,
+    nb_visit_es INTEGER DEFAULT 0
 );
 
 -- Matomo tables
@@ -60,12 +105,20 @@ CREATE TABLE IF NOT EXISTS metric.matomo_organizations
     organization_id CHARACTER VARYING,
     nb_outlink INTEGER
 );
+CREATE TABLE IF NOT EXISTS metric.matomo_dataservices
+(
+    __id SERIAL PRIMARY KEY,
+    date_metric DATE,
+    dataservice_id CHARACTER VARYING,
+    organization_id CHARACTER VARYING,
+    nb_outlink INTEGER
+);
 
 
 -- Aggregated metrics tables
 CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_datasets AS
     SELECT visits.__id as __id,
-            COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
+           COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
            COALESCE(visits.dataset_id, matomo.dataset_id) as dataset_id,
            COALESCE(visits.organization_id, matomo.organization_id) as organization_id,
            visits.nb_visit,
@@ -97,6 +150,19 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_reuses AS
        visits.date_metric = matomo.date_metric
 ;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_dataservices AS
+    SELECT visits.__id as __id,
+           COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
+           COALESCE(visits.dataservice_id, matomo.dataservice_id) as dataservice,
+           COALESCE(visits.organization_id, matomo.organization_id) as organization_id,
+           visits.nb_visit,
+           matomo.nb_outlink
+    FROM metric.visits_dataservices AS visits
+    FULL OUTER JOIN metric.matomo_dataservices AS matomo
+    ON visits.dataservice_id = matomo.dataservice_id AND
+       visits.date_metric = matomo.date_metric
+;
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_organizations AS
     SELECT visits.__id as __id,
            COALESCE(visits.date_metric, matomo.date_metric) as date_metric,
@@ -122,6 +188,12 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.metrics_organizations AS
     ) reuses
     ON COALESCE(visits.organization_id, matomo.organization_id) = reuses.organization_id AND
        COALESCE(visits.date_metric, matomo.date_metric) = reuses.date_metric
+    LEFT OUTER JOIN (
+        SELECT organization_id, date_metric, sum(nb_visit) as nb_visit FROM metric.metrics_dataservices
+        GROUP BY organization_id, date_metric
+    ) dataservices
+    ON COALESCE(visits.organization_id, matomo.organization_id) = dataservices.organization_id AND
+       COALESCE(visits.date_metric, matomo.date_metric) = dataservices.date_metric
 ;
 
 -- Monthly aggregated metrics tables
@@ -169,6 +241,16 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.resources AS
     GROUP BY metric_month, resource_id, dataset_id
 ;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.dataservices AS
+    SELECT
+        MIN(__id) as __id,
+        dataservice_id,
+        to_char(date_trunc('month', date_metric) , 'YYYY-mm') AS metric_month,
+        sum(nb_visit) as monthly_visit
+    FROM metric.visits_dataservices
+    GROUP BY metric_month, dataservice_id
+;
+
 -- Global site table
 CREATE MATERIALIZED VIEW IF NOT EXISTS metric.site AS
     SELECT __id,
@@ -187,6 +269,11 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.site AS
         sum(monthly_visit) as monthly_visit
         FROM metric.reuses GROUP BY metric_month ) reuses
     ON datasets.metric_month = reuses.metric_month
+    FULL OUTER JOIN (
+        SELECT metric_month,
+        sum(monthly_visit) as monthly_visit
+        FROM metric.dataservices GROUP BY metric_month ) dataservices
+    ON datasets.metric_month = dataservices.metric_month
 ;
 
 -- Sum tables
@@ -233,6 +320,14 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS metric.resources_total AS
     GROUP BY resource_id, dataset_id
 ;
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS metric.dataservices_total AS
+    SELECT
+        MIN(__id) AS __id,
+        dataservice_id,
+        SUM(nb_visit) AS visit_dataservice
+    FROM metric.visits_dataservices
+    GROUP BY dataservice_id
+;
 
 CREATE INDEX IF NOT EXISTS visits_datasets_dataset_id ON metric.visits_datasets USING btree (dataset_id);
 CREATE INDEX IF NOT EXISTS visits_datasets_date_metric ON metric.visits_datasets USING btree (date_metric);
@@ -248,3 +343,7 @@ CREATE INDEX IF NOT EXISTS visits_reuses_organization_id ON metric.visits_reuses
 CREATE INDEX IF NOT EXISTS visits_resources_resource_id ON metric.visits_resources USING btree (resource_id);
 CREATE INDEX IF NOT EXISTS visits_resources_date_metric ON metric.visits_resources USING btree (date_metric);
 CREATE INDEX IF NOT EXISTS visits_resources_dataset_id ON metric.visits_resources USING btree (dataset_id);
+
+CREATE INDEX IF NOT EXISTS visits_dataservices_dataservice_id ON metric.visits_dataservices USING btree (dataservice_id);
+CREATE INDEX IF NOT EXISTS visits_dataservices_organization_id ON metric.visits_dataservices USING btree (organization_id);
+CREATE INDEX IF NOT EXISTS visits_dataservices_date_metric ON metric.visits_dataservices USING btree (date_metric);